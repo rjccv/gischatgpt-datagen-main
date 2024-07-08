37288802e1c5f23dfa515b7f1ce06e2990676c67
roncamposj <roncamposj@knights.ucf.edu>
2024-07-07 20:16:07 -0400

    .

---
+++ gen_qa.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sbatch gen_qa1.slurm
+sbatch gen_qa2.slurm
+sbatch gen_qa3.slurm
+sbatch gen_qa4.slurm
---
+++ osm_calls.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sbatch osm1.slurm
+sbatch osm2.slurm
+sbatch osm3.slurm
+sbatch osm4.slurm
